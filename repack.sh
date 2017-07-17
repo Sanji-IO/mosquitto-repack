7119ac72f02f816ece6a5dd6ab1ed1dc992f9390
Zack YL Shih <zackyl.shih@moxa.com>
2017-07-17 14:10:28 +0800

    Update to mosquitto_1.4.14-0mosquitto1~jessie1

--- repack.sh
+++ repack.sh
@@ -3,9 +3,9 @@ set -x
 set -e
 
 rm -rf tmp
-fakeroot sh -c 'dpkg-deb -R deb/mosquitto_1.4.12-0mosquitto1_armhf.deb tmp'
+fakeroot sh -c 'dpkg-deb -R deb/mosquitto_1.4.14-0mosquitto1~jessie1_armhf.deb tmp'
 rm -rf tmp/lib/systemd/system/mosquitto.service
 cp src/DEBIAN/control tmp/DEBIAN
 
-fakeroot sh -c 'dpkg-deb -b tmp mosquitto_1.4.12+moxa_armhf.deb'
+fakeroot sh -c 'dpkg-deb -b tmp mosquitto_1.4.14-0mosquitto1~jessie1+moxa_armhf.deb'
 
